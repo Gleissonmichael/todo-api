a0e0603d52eb4a2d8d9c12017312a0ef2f8663ab
Gleisson Santos <gleissonmichael1@gmail.com>
2023-08-07 21:57:24 -0300

    fix: header access control

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
 
   app.enableCors();
+  
+  app.use((res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
 
   //start listening
   await app.listen(8080);
