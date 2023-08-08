cf63163423903e4980117e373efab1f9c75d0ea3
Gleisson Santos <gleissonmichael1@gmail.com>
2023-08-07 22:13:56 -0300

    fix: config cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 import { APP_FILTER } from "@nestjs/core";
 
-import { Module, OnModuleInit } from "@nestjs/common";
-
+import { Module, OnModuleInit, MiddlewareConsumer } from "@nestjs/common";
+ 
+import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from "@nestjs/config";
 import { SessionsModule } from "./modules/sessions.module";
 import { CustomErrorFilter } from "./controllers/filters/custom.error.filter";
@@ -17,6 +18,7 @@ dotenv.config();
     ConfigModule.forRoot({ envFilePath: [".env"], isGlobal: true }),
     SessionsModule,
     TodoModule,
+    HttpModule
   ],
   exports: [SessionsModule, TodoModule],
   controllers: [],
@@ -29,5 +31,20 @@ dotenv.config();
 })
 export class AppModule implements OnModuleInit {
   constructor() {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+      })
+      .forRoutes("*");
+  }
+
   async onModuleInit() {}
 }
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,6 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
-    next();
-  });
-
   app.enableCors();
 
   //start listening
