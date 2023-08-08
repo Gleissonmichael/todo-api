61562713f8cbfed14a4fe59a2a2e20ed01123db4
Gleisson Santos <gleissonmichael1@gmail.com>
2023-08-07 22:01:10 -0300

    fix: headers

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
 
-  app.enableCors();
-  
-  app.use((res, next) => {
+  app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
     next();
   });
+
+  app.enableCors();
 
   //start listening
   await app.listen(8080);
