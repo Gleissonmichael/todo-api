701175d9c6e47465df28379c35af42076a9aec8c
Gleisson Santos <gleissonmichael1@gmail.com>
2023-08-10 19:23:55 -0300

    fix: cors no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ export class AppModule implements OnModuleInit {
     consumer
       .apply((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
         res.header(
           "Access-Control-Allow-Headers",
           "Origin, X-Requested-With, Content-Type, Accept"
